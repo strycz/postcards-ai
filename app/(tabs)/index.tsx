82dcbc27efb46313ed6bfd3fca80b30cd59b49a8
Pawel <stryczniewicz@gmail.com>
2024-08-19 06:55:13 +0200

    before moving to separate components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,18 @@
-import { Check, ExternalLink, Trash } from '@tamagui/lucide-icons';
+import { Check, Trash } from '@tamagui/lucide-icons';
 import {
-  Anchor,
   Button,
   Checkbox,
   Form,
   H2,
   H4,
   Input,
-  Paragraph,
+  ScrollView,
   Spinner,
   Text,
   XStack,
   YStack,
 } from 'tamagui';
+import { LinearGradient } from 'tamagui/linear-gradient';
 import { ToastControl } from 'app/CurrentToast';
 import { useMutation, useQuery } from 'convex/react';
 import { api } from 'convex/_generated/api';
@@ -48,127 +48,102 @@ export default function TabOneScreen() {
   };
 
   return (
-    <YStack f={1} ai="center" gap="$8" px="$10" pt="$5" backgroundColor="$blue1">
-      <XStack gap="$4">
-        <ToastControl />
-      </XStack>
+    <YStack f={1} ai="center" backgroundColor="$blue1">
+      <ScrollView showsHorizontalScrollIndicator={false} p="$4" br="$4">
+        <YStack ai="center" gap="$8" width="100%">
+          <H2 color="$blue10">Task Manager</H2>
 
-      <H2 color="$blue10">Task Manager</H2>
-
-      <XStack gap="$4" width="100%">
-        <Form
-          alignItems="center"
-          width="100%"
-          gap="$4"
-          onSubmit={() => setStatus('submitting')}
-          borderWidth={2}
-          borderRadius="$6"
-          backgroundColor="$blue2"
-          borderColor="$blue5"
-          padding="$6"
-        >
           <XStack gap="$4" width="100%">
-            <Input
-              flex={1}
-              value={input}
-              onChangeText={setInput}
-              placeholder="Add a task"
-              borderColor="$blue7"
-              focusStyle={{ borderColor: '$blue10' }}
-            />
-            <Button
-              onPress={addTask}
-              backgroundColor="$green8"
-              color="white"
-              pressStyle={{ backgroundColor: '$green9' }}
+            <Form
+              alignItems="center"
+              width="fit-content"
+              height="fit-content"
+              gap="$4"
+              onSubmit={() => setStatus('submitting')}
+              borderWidth={2}
+              borderRadius="$6"
+              backgroundColor="$blue2"
+              borderColor="$blue5"
+              padding="$6"
             >
-              Add
-            </Button>
+              <XStack gap="$4">
+                <Input
+                  value={input}
+                  onChangeText={setInput}
+                  placeholder="Add a task"
+                  borderColor="$blue7"
+                  focusStyle={{ borderColor: '$blue10' }}
+                />
+                <Button
+                  onPress={addTask}
+                  backgroundColor="$green8"
+                  color="white"
+                  pressStyle={{ backgroundColor: '$green9' }}
+                >
+                  Add
+                </Button>
+              </XStack>
+
+              <Form.Trigger asChild disabled={status !== 'off'}>
+                <Button
+                  icon={status === 'submitting' ? () => <Spinner /> : undefined}
+                  backgroundColor="$blue8"
+                  color="white"
+                  pressStyle={{ backgroundColor: '$blue9' }}
+                >
+                  Submit
+                </Button>
+              </Form.Trigger>
+            </Form>
+
+            <YStack gap="$4" width="50%">
+              <H4 color="$blue10">Your Tasks</H4>
+              {tasks?.map(({ _id, text }) => (
+                <XStack
+                  key={_id}
+                  alignItems="center"
+                  justifyContent="space-between"
+                  gap="$4"
+                  borderWidth={1}
+                  borderRadius="$4"
+                  borderColor="$blue5"
+                  backgroundColor="$blue2"
+                  padding="$4"
+                >
+                  <XStack alignItems="center" gap="$2" flex={1}>
+                    <Checkbox
+                      id={_id}
+                      size="$4"
+                      defaultChecked={true}
+                      onCheckedChange={() => sendLike({ liker: text, messageId: _id })}
+                    >
+                      <Checkbox.Indicator>
+                        <Check color="$green10" />
+                      </Checkbox.Indicator>
+                    </Checkbox>
+
+                    <Text color="$gray11" flex={1}>
+                      {text}
+                    </Text>
+                  </XStack>
+
+                  <Button
+                    padding="$2"
+                    hoverStyle={{
+                      scale: 1.1,
+                    }}
+                    icon={<Trash color="$red10" />}
+                    backgroundColor="$red2"
+                    pressStyle={{ backgroundColor: '$red3' }}
+                  >
+                    Delete
+                  </Button>
+                </XStack>
+              ))}
+            </YStack>
           </XStack>
-
-          <Form.Trigger asChild disabled={status !== 'off'}>
-            <Button
-              icon={status === 'submitting' ? () => <Spinner /> : undefined}
-              backgroundColor="$blue8"
-              color="white"
-              pressStyle={{ backgroundColor: '$blue9' }}
-            >
-              Submit
-            </Button>
-          </Form.Trigger>
-        </Form>
-      </XStack>
-
-      <YStack gap="$4" width="100%">
-        <H4 color="$blue10">Your Tasks</H4>
-        {tasks?.map(({ _id, text }) => (
-          <XStack
-            key={_id}
-            alignItems="center"
-            justifyContent="space-between"
-            gap="$4"
-            borderWidth={1}
-            borderRadius="$4"
-            borderColor="$blue5"
-            backgroundColor="$blue2"
-            padding="$4"
-          >
-            <XStack alignItems="center" gap="$2" flex={1}>
-              <Checkbox
-                id={_id}
-                size="$4"
-                defaultChecked={true}
-                onCheckedChange={() => sendLike({ liker: text, messageId: _id })}
-              >
-                <Checkbox.Indicator>
-                  <Check color="$green10" />
-                </Checkbox.Indicator>
-              </Checkbox>
-
-              <Text color="$gray11" flex={1}>
-                {text}
-              </Text>
-            </XStack>
-
-            <Button
-              padding="$2"
-              icon={<Trash color="$red10" />}
-              backgroundColor="$red2"
-              pressStyle={{ backgroundColor: '$red3' }}
-            >
-              Delete
-            </Button>
-          </XStack>
-        ))}
-      </YStack>
-
-      <XStack ai="center" jc="center" fw="wrap" gap="$3" pos="absolute" b="$8">
-        <Paragraph fos="$4" color="$gray11">
-          Need help? Check out our
-        </Paragraph>
-
-        <XStack
-          ai="center"
-          gap="$2"
-          px="$3"
-          py="$2"
-          br="$4"
-          bg="$purple5"
-          hoverStyle={{ bg: '$purple6' }}
-          pressStyle={{ bg: '$purple4' }}
-        >
-          <Anchor
-            href="https://tamagui.dev/docs/core/configuration"
-            textDecorationLine="none"
-            col="$purple10"
-            fos="$4"
-            fontWeight="bold"
-          >
-            Configuration guide
-          </Anchor>
-          <ExternalLink size="$2" col="$purple10" />
-        </XStack>
-      </XStack>
+        </YStack>
+      </ScrollView>
     </YStack>
   );
 }
